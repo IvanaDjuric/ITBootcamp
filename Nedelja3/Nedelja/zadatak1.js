02998d8868cd40d37a0e488359d2c6af233d0582
IvanaDjuric <iwanadjrc@yahoo.com>
2020-08-01 22:40:44 +0200

    dodat fajl

---
+++ Nedelja3/Nedelja/zadatak1.js
@@ -0,0 +1,11 @@
+
+function divisibleByFive(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 5 === 0) {
+            console.log(arr[i])
+      }
+        
+    }
+}
+
+divisibleByFive([1,2,3,4,5,10,15,23,1,5])
---
+++ Nedelja3/Nedelja/zadatak2.js
@@ -0,0 +1,8 @@
+
+function lifeSupply(numPerMonth,age) {
+  
+    console.log((100 - age) * 12 * numPerMonth)
+        
+}
+
+lifeSupply(10,26)
