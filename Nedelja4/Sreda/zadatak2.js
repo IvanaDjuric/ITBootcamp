41cf33874577c81fa1989c32aaa009e52436df7c
IvanaDjuric <iwanadjrc@yahoo.com>
2020-08-04 23:12:51 +0200

    dodat fajl

---
+++ Nedelja4/Sreda/zadatak2.js
@@ -0,0 +1,59 @@
+
+let pokemon1 = {
+    name : 'Pikachu',
+    species : 'electric',
+    abilities : ['quick attack', 'tunderbolt', 'volt tackle'],
+    chars : {
+            attack : 112,
+            defense : 96,
+            stamina : 111
+    }
+}
+
+let pokemon2 = {
+    name : 'Pidgey',
+    species : 'flying',
+    abilities : ['quick attack', 'tackle', 'aerial ice'],
+    chars : {
+        attack : 85,
+        defense : 73,
+        stamina : 120
+    }
+}
+
+let pokemon3 = {
+    name : 'Charmeleon',
+    species : 'fire',
+    abilities : ['fire fang', 'scratch', 'ember'],
+    chars : {
+        attack : 158,
+        defense : 126,
+        stamina : 151
+    }
+}
+
+let pokemon4 = {
+    name : 'Poliwag',
+    species : 'water',
+    abilities : ['bubble', 'bubble beam', 'mud shot'],
+    chars : {
+        attack : 101,
+        defense : 82,
+        stamina : 121
+    }
+}
+
+let pokemons = [pokemon1, pokemon2, pokemon3, pokemon4]
+
+
+
+function sposobnosti(pokemons) {
+    let niz = []
+   
+    pokemons.forEach(element => {
+        niz.push(element.abilities)
+        
+    }) 
+    return niz
+}
+console.log(sposobnosti(pokemons))
